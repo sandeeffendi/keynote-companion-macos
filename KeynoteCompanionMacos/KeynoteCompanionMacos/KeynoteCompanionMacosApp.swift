8659cd6823a62eab13cb3f99469cec6a8dbf0bcd
Sande Effendi <sande.effendi@gmail.com>
2026-05-26 09:35:26 +0700

    initial commit

---
+++ KeynoteCompanionMacos/KeynoteCompanionMacos/KeynoteCompanionMacosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeynoteCompanionMacosApp.swift
+//  KeynoteCompanionMacos
+//
+//  Created by Sande Effendi on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct KeynoteCompanionMacosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
