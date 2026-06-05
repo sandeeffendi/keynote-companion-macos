603f49de1d864b5da7eda12112c00264e6d64bb0
Fajar Ahmad Kurniadi <fajarsigmachad@gmail.com>
2026-06-05 09:46:48 +0700

    menambahkan component design token untuk spacing

--- KeynoteCompanionMacos/Core/DesignSystem/AppSpacing.swift
+++ KeynoteCompanionMacos/Core/DesignSystem/AppSpacing.swift
@@ -9,5 +9,11 @@ import Foundation
 
 // PIC: fajar
 enum AppSpacing {
-
+    static let tight: CGFloat = 4
+    static let cozy: CGFloat = 8
+    static let comfortable: CGFloat = 12
+    static let roomy: CGFloat = 16
+    static let spacious: CGFloat = 24
+    static let generous: CGFloat = 32
+    static let expansive: CGFloat = 36
 }
